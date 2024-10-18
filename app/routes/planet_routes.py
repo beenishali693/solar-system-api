c02f9bfab8e2cff3dd1a31132966835844873916
Wei Qiang <qwbu22@gmail.com>
2024-10-18 12:24:38 -0700

    draft 2

--- app/routes/planet_routes.py
+++ app/routes/planet_routes.py
@@ -1,9 +1,20 @@
 from flask import Blueprint
 from ..models.planets import planets
 
-planets_bp = Blueprint("planets",__name__,url_prefix="/planets")
+planets_bp = Blueprint("planets_bp",__name__,url_prefix="/planets")
 
 @planets_bp.get("")
-def get_planets():
-    return planets
+def get_all_planets():
+    result_list = []
+
+    for planet in planets:
+        result_list.append(dict(
+            id=planet.id,
+            name=planet.name,
+            description=planet.description,
+            galaxy=planet.galaxy
+        ))
+    return result_list
+
+    
 
