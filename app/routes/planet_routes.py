2c4dedc66d43e5367b3463ca0d8aa2a7c0756d53
Wei Qiang <qwbu22@gmail.com>
2024-10-23 22:21:38 -0700

    merge wave2

--- app/routes/planet_routes.py
+++ app/routes/planet_routes.py
@@ -1,4 +1,4 @@
-from flask import Blueprint
+from flask import Blueprint,abort,make_response
 from ..models.planet import planets
 
 planets_bp = Blueprint("planets_bp",__name__,url_prefix="/planets")
@@ -17,3 +17,26 @@ def get_all_planets():
         )
     return planets_response
 
+@planets_bp.get("/<planet_id>")
+def get_one_plane(planet_id):
+    planet = validate_planet(planet_id)
+
+    return {
+        "id": planet.id,
+        "name": planet.name,
+        "description": planet.description,
+        "galaxy": planet.galaxy
+    }
+
+def validate_planet(planet_id):
+    try:
+        planet_id = int(planet_id)
+    except ValueError:
+        response = f"message: planet {planet_id} invalid"
+        abort(make_response(response,400))
+    
+    for planet in planets:
+        if planet.id == planet_id:
+            return planet
+    response = f"message: planet {planet_id} not found"
+    abort(make_response(response,404))
